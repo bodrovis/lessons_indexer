c9fbda822bb04062233a21780b0bc3085dbbfaa5
bodrovis <ibodrov@campaigner.ru>
2015-06-10 20:19:38 +0300

    minor fixes

--- lib/lessons_indexer/course.rb
+++ lib/lessons_indexer/course.rb
@@ -31,8 +31,8 @@ module LessonsIndexer
 
     def generate_pdfs
       within(dir, true) do
-        lessons.list.sort.first(2).each do |lesson|
-          %x{pandoc #{lesson.file_name} -f markdown_github -o #{lesson.file_name.gsub(/md\z/i, '')}.pdf --variable geometry:"top=1.5cm, bottom=2.5cm, left=1.5cm, right=1.5cm" --latex-engine=xelatex --variable mainfont="Open Sans" --variable monofont="Liberation Mono" --variable fontsize="12pt"}
+        lessons.list.sort.each do |lesson|
+          %x{pandoc #{lesson.file_name} -f markdown_github -o #{lesson.file_name.gsub(/md\z/i, '')}pdf --variable geometry:"top=1.5cm, bottom=2.5cm, left=1.5cm, right=1.5cm" --latex-engine=xelatex --variable mainfont="Open Sans" --variable monofont="Liberation Mono" --variable fontsize="12pt"}
         end
       end
     end
